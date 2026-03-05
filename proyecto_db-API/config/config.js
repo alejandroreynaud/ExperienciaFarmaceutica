9cf6e813f9ddba4beddb91237bcf7238bdf6499a
nicky18-txt <nic.altamirano@unitec.edu>
2026-03-05 10:15:27 -0600

    Correciones en la configuracion de Docker

--- proyecto_db-API/config/config.js
+++ proyecto_db-API/config/config.js
@@ -11,11 +11,11 @@ const{
 module.exports ={
 
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: DB_DIALECT
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   },
   test: {
     username: process.env.DB_USERNAME,
