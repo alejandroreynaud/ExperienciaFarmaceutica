87fdbcc582d31ce29dc866e2f75ce6fdfbcc28d7
nicky18-txt <nic.altamirano@unitec.edu>
2026-03-12 13:28:08 -0600

    Creacion de endpoint post /productos en el que se ingresa el nombre y la imagen del producto

--- proyecto_db-API/controllers/productos.js
+++ proyecto_db-API/controllers/productos.js
@@ -1,1 +1,37 @@
 //archivo para hacer endpoints de productos
+
+const { Producto } = require('../models');
+
+// Nuevo producto
+let createProducto = async (request, response) => {
+    try {
+        const { nombre, imagen } = request.body;
+
+        if (nombre === undefined || imagen === undefined ) {
+            return response.status(400).json({ error: 'Faltan campos obligatorios' });
+        }
+
+        let productoExistente = await Producto.findOne({ where: { nombre } });
+        if (productoExistente) {
+            return response.status(409).json({ error: 'El producto ya existe' });
+        }
+
+        let nuevoProducto = await Producto.create(request.body);
+        response.status(201).json({ 
+            message: 'Producto creado exitosamente',
+            status: 201,
+            data: nuevoProducto
+         });
+
+    } catch (error) {
+        console.error('Error al crear producto:', error);
+        response.status(500).json({ 
+            message: 'Error interno del servidor',
+            status: 500,
+            error: error.message});
+    }   
+}
+
+module.exports = {
+    createProducto
+}
