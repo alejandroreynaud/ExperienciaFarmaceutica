df7c5fa6dfbef2f82c380f46b0451224071e44f6
hectorc10 <hectorcarranza509@gmail.com>
2026-03-12 12:14:16 -0600

    Endpoint post factura

--- proyecto_db-API/routes/facturas.js
+++ proyecto_db-API/routes/facturas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const facturasController = require('../controllers/facturasController');
 router.post('/facturas', facturasController.createFactura);
-router.get('/facturas/fecha', facturasController.getFacturasPorFecha);
+router.get('/facturas/fecha', facturasController.getFacturasFecha);
 router.get('/facturas/:id', facturasController.getFacturaId);
 
 module.exports = router;
