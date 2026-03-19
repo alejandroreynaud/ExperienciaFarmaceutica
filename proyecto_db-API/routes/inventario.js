01d1f4ab5e680f58d89446ba7bf975eadf7cd2e7
nicky18-txt <nic.altamirano@unitec.edu>
2026-03-19 13:13:22 -0600

    Creacion de endpoint para hacer un reporte del stock total de productos

--- proyecto_db-API/routes/inventario.js
+++ proyecto_db-API/routes/inventario.js
@@ -8,5 +8,6 @@ router.get('/inventario', inventarioController.getInventarios);
 router.get('/inventario/:codigo', inventarioController.getInventarioByCodigo);
 router.put('/inventario/:id', inventarioController.updateInventario);
 router.get('/inventario/alertas/vencer', inventarioController.getAlertasPorVencer);
+router.get('/inventario/reporte/stock-total', inventarioController.getReporteStockTotal);
 
 module.exports = router;
