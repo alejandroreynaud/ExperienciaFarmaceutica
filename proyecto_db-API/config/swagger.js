7919a377dd959faa2b2348a4921d84dbe955649a
Alejandro Reynaud <alejandroreynaud.15@gmail.com>
2026-03-22 13:39:33 -0600

    Swaggers

--- proyecto_db-API/config/swagger.js
+++ proyecto_db-API/config/swagger.js
@@ -13,16 +13,22 @@ if (fs.existsSync(pathsDir)) {
   swaggerDocument.paths = swaggerDocument.paths || {};
 
   pathFiles.forEach((fileName) => {
-    const routeName = `/${path.basename(fileName, '.yaml')}`;
-    swaggerDocument.paths[routeName] = YAML.load(path.join(pathsDir, fileName));
+    const filePath = path.join(pathsDir, fileName);
+    const fileContent = YAML.load(filePath);
+
+    swaggerDocument.paths = {
+      ...swaggerDocument.paths,
+      ...fileContent
+    };
   });
 }
+
 
 const swaggerSetup = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
     explorer: true,
-    customSiteTitle: "Experiencia Farmaceutica API Documentation"
+    customSiteTitle: "API Documentation"
   }));
-}
+};
 
 module.exports = swaggerSetup;
--- proyecto_db-API/routes/clientes.js
+++ proyecto_db-API/routes/clientes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const clientesController = require("../controllers/clientesController");
+const { getClienteById, createCliente, updateCliente } = require("../controllers/clientesController");
 
 router.post("/clientes", clientesController.createCliente);
 router.get("/clientes/:id", clientesController.getClienteById);
