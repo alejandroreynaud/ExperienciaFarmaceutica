279866de1409077bbb5205b5ffa9ddeb127c5a41
nicky18-txt <nic.altamirano@unitec.edu>
2026-03-26 13:16:26 -0600

    cambios

--- proyecto_db-API/app.js
+++ proyecto_db-API/app.js
@@ -61,7 +61,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const clientesRoutes = require("./routes/clientes");
-app.use("/api", clientesRoutes);
-
 module.exports = app;
