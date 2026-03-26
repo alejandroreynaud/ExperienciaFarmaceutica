bd1cb6d0e34e060d02bb2156c04d0b0b87492fcd
hectorc10 <hectorcarranza509@gmail.com>
2026-03-26 09:07:16 -0600

    Conexion back con front ventas

--- proyecto_db-API/routes/ventas.js
+++ proyecto_db-API/routes/ventas.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const ventasController = require("../controllers/ventasController");
-
+const { route } = require("./facturas");
+router.get("/ventas/productos", ventasController.getProductosVenta);
 router.get("/ventas", ventasController.getVentasFecha);
 router.get("/ventas/cliente/:id_cliente", ventasController.getVentasByCliente);
 router.get("/ventas/:id", ventasController.getVentaById);
@@ -9,5 +10,6 @@ router.post("/ventas", ventasController.createVenta);
 router.get("/semanal", ventasController.getVentasSemanal);
 router.get("/mensual", ventasController.getVentasMensual);
 router.get("/hoy", ventasController.getVentasHoy);
+
 
 module.exports = router;
