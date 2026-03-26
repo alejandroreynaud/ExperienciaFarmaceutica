d44dc56ed142f76af9918e97da88e83d0a79ce1c
nicky18-txt <nic.altamirano@unitec.edu>
2026-03-26 11:07:24 -0600

    push

--- proyecto_db-API/app.js
+++ proyecto_db-API/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const swaggerSetup = require('./config/swagger');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ventasRoutes = require("./routes/ventas");
